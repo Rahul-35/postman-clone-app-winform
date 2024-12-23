1cc64602c80b3893430cfe2a23ac6b997b97a46f
Rahul Ch <xavi3441@gmail.com>
2024-12-23 23:36:55 +0530

    Designed the UI

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace PostmanCloneUI
+﻿
+namespace PostmanCloneUI
 {
     partial class Dashboard
     {
@@ -28,12 +29,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            formHeader = new Label();
+            apiLabel = new Label();
+            apiText = new TextBox();
+            apiClick = new Button();
+            resultsText = new TextBox();
+            statusStrip = new StatusStrip();
+            systemStatus = new ToolStripStatusLabel();
+            resultsLabel = new Label();
+            statusStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // formHeader
+            // 
+            formHeader.AutoSize = true;
+            formHeader.Font = new Font("Segoe UI", 19F);
+            formHeader.Location = new Point(58, 18);
+            formHeader.Name = "formHeader";
+            formHeader.Size = new Size(234, 45);
+            formHeader.TabIndex = 0;
+            formHeader.Text = "Postman Clone";
+            // 
+            // apiLabel
+            // 
+            apiLabel.AutoSize = true;
+            apiLabel.Font = new Font("Segoe UI", 16F);
+            apiLabel.Location = new Point(58, 72);
+            apiLabel.Name = "apiLabel";
+            apiLabel.Size = new Size(62, 37);
+            apiLabel.TabIndex = 1;
+            apiLabel.Text = "API:";
+            // 
+            // apiText
+            // 
+            apiText.Font = new Font("Segoe UI", 12F);
+            apiText.Location = new Point(112, 75);
+            apiText.Name = "apiText";
+            apiText.Size = new Size(810, 34);
+            apiText.TabIndex = 2;
+            // 
+            // apiClick
+            // 
+            apiClick.BackColor = Color.FromArgb(255, 128, 0);
+            apiClick.Font = new Font("Segoe UI", 12F);
+            apiClick.Location = new Point(938, 72);
+            apiClick.Name = "apiClick";
+            apiClick.Size = new Size(94, 43);
+            apiClick.TabIndex = 3;
+            apiClick.Text = "Go";
+            apiClick.UseVisualStyleBackColor = false;
+            apiClick.Click += apiClick_Click;
+            // 
+            // resultsText
+            // 
+            resultsText.BackColor = Color.White;
+            resultsText.Font = new Font("Segoe UI", 14F);
+            resultsText.Location = new Point(58, 169);
+            resultsText.Multiline = true;
+            resultsText.Name = "resultsText";
+            resultsText.ReadOnly = true;
+            resultsText.ScrollBars = ScrollBars.Both;
+            resultsText.Size = new Size(1058, 438);
+            resultsText.TabIndex = 4;
+            // 
+            // statusStrip
+            // 
+            statusStrip.ImageScalingSize = new Size(20, 20);
+            statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+            statusStrip.Location = new Point(0, 618);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(1217, 34);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "statusStrip";
+            // 
+            // systemStatus
+            // 
+            systemStatus.Font = new Font("Segoe UI", 12F);
+            systemStatus.Name = "systemStatus";
+            systemStatus.Size = new Size(65, 28);
+            systemStatus.Text = "Ready";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.Font = new Font("Segoe UI", 16F);
+            resultsLabel.Location = new Point(58, 129);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(99, 37);
+            resultsLabel.TabIndex = 6;
+            resultsLabel.Text = "Results";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1217, 652);
+            Controls.Add(resultsLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(resultsText);
+            Controls.Add(apiClick);
+            Controls.Add(apiText);
+            Controls.Add(apiLabel);
+            Controls.Add(formHeader);
+            Font = new Font("Segoe UI", 18F);
+            ForeColor = SystemColors.ControlText;
+            Margin = new Padding(6);
+            Name = "Dashboard";
+            Text = "Postman Clone by RCH";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label formHeader;
+        private Label apiLabel;
+        private TextBox apiText;
+        private Button apiClick;
+        private TextBox resultsText;
+        private StatusStrip statusStrip;
+        private Label resultsLabel;
+        private ToolStripStatusLabel systemStatus;
     }
 }
--- PostmanCloneUI/Dashboard.cs
+++ PostmanCloneUI/Dashboard.cs
@@ -6,5 +6,23 @@ namespace PostmanCloneUI
         {
             InitializeComponent();
         }
+
+        private async void apiClick_Click(object sender, EventArgs e)
+        {
+
+            //validate api url
+            try
+            {
+                systemStatus.Text = "Calling API...";
+                //sample code replace with actual API
+                await Task.Delay(1000);
+                systemStatus.Text = "Ready";
+            }
+            catch (Exception ex) 
+            {
+                resultsText.Text = "Error: "+ex.Message;
+                systemStatus.Text = "Error";
+            }
+        }
     }
 }
